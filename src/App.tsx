fa6eac5a34de818d71bd689c2b3289f03ae2ab5f
MENG-HAN WU <emhw.0930@berkeley.edu>
2025-05-03 16:20:17 -0700

    Switch to HashRouter and update sitemap.xml for hash-based routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate, useLocation } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route, Navigate, useLocation } from 'react-router-dom';
 import './App.css';
 import ChineseTypingTest from './components/ChineseTypingTest';
 import EnglishTypingTest from './components/EnglishTypingTest';
